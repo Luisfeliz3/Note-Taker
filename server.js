b7cbb25d31d79710517b8395c139c501f09c58da
Luis Feliz <Luisfeliz@gmail.com>
2020-10-22 14:07:03 -0400

    choer(NT):adding files structure

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+const fs = require("fs");
+
+// Create Express server
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app, fs);
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("http://localhost:" + PORT);
+});
+
