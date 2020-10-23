ab82725048e1936a16554029f93f37fbfd3aca2a
Luis Feliz <Luisfeliz@gmail.com>
2020-10-23 14:27:34 -0400

    chore(NT): adding logic refactor vars and paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,57 +1,48 @@
 // API routes
 
-module.exports = function(app, fs) {
-
+module.exports = function (app, fs) {
   let notesDB = require("../db/db.json");
   let dataPath = "./db/db.json";
   // read file
   app.get("/api/notes", (req, res) => {
-      fs.readFile(dataPath, "utf8", (err, data) => {
-          if (err) {
-          throw err;
+    fs.readFile(dataPath, "utf8", (err, data) => {
+      if (err) {
+        throw err;
       }
       res.send(JSON.parse(data));
-      });
+    });
   });
 
   app.post("/api/notes", (req, res) => {
-      
-      var newNote = req.body;
-      var id = (notesDB.length).toString();
-      newNote.id = id;
-
-      notesDB.push(newNote);
-      
-      // res.json(true);
-      res.json(newNote);
-
-      console.log(notesDB);
-
-      fs.writeFileSync(dataPath, JSON.stringify(notesDB, null, 2));
-      res.json(notesDB);
+    var newNote = req.body;
+    var id = notesDB.length.toString();
+    newNote.id = id;
+    notesDB.push(newNote);
+    res.json(newNote);
+    fs.writeFileSync(dataPath, JSON.stringify(notesDB, null, 2));
+    res.json(notesDB);
   });
 
-app.delete("/api/notes/:id", (req, res) => {
+  app.delete("/api/notes/:id", (req, res) => {
+    let savedNotes = JSON.parse(
+      fs.readFileSync(dataPath, "utf8", (err) => {
+        if (err) throw err;
+      })
+    );
 
-let savedNotes = JSON.parse(fs.readFileSync(dataPath, "utf8",err => {
-if (err) throw err;
+    let noteID = req.params.id;
+    let newID = 0;
 
-}));
+    savedNotes = savedNotes.filter((currentNote) => {
+      return currentNote.id != noteID;
+    });
 
-
-let noteID = req.params.id;
-let newID = 0;
-
-savedNotes = savedNotes.filter(currentNote => {
-return currentNote.id != noteID;
-})
-
-for (currentNote of savedNotes) {
+    for (currentNote of savedNotes) {
       currentNote.id = newID.toString();
       newID++;
-}
+    }
 
-  fs.writeFileSync(dataPath, JSON.stringify(savedNotes, null, 2));
-  res.json(savedNotes);
-  })
-}
+    fs.writeFileSync(dataPath, JSON.stringify(savedNotes, null, 2));
+    res.json(savedNotes);
+  });
+};
